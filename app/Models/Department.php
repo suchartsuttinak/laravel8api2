da507e4a480f0370db0b32feeb124f1c5c40c340
suchart <suchartsuttinak@gmail.com>
2021-06-18 22:17:59 +0700

    department_route

---
+++ app/Models/Department.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    use HasFactory;
+    protected $table = 'department';
+   // protected $primaryKey = 'd_id'; //กรณี  pk เป็นชื่ออื่น
+  // protected $keyType = 'string'; // กรณี pk = vachar
+  // public $incrementing = false; // pk != auto_increment
+ // public $timestramps = false; // ตารางไม่มีคอลั่มน์  created_ad ในตาราง
+}
